91b9b54c44c66e438b99b7cf313699b15ca06570
playLoL <playza.1987@gmail.com>
2026-02-10 14:37:44 +0700

    lab17_3.cpp completed

--- lab17_3.cpp
+++ lab17_3.cpp
@@ -5,12 +5,15 @@
 using namespace std;
 
 vector<int> randomVector(unsigned int N){	
+
     vector<int> v;
     for(unsigned int i = 0; i < N; i++) v.push_back(rand()%10);
     return v;
+
 }
 
 vector<int> showVector(const vector<int> &v){
+
 	cout << "[";
 	for(unsigned int i = 0; i < v.size(); i++){
 		cout << v[i];
@@ -18,12 +21,15 @@ vector<int> showVector(const vector<int> &v){
 		else cout << " ";
 	}
 	return v;
+
 }
 
 int dotProduct(const vector<int> &x, const vector<int> &y){
+
 	int sum = 0;
 	for(unsigned int i = 0; i < x.size(); i++) sum += x[i]*y[i];	
 	return sum;
+	
 }
 
 int main(){
