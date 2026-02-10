03d438e6b35330144b5e6ad4ecf27816725f2dec
playLoL <playza.1987@gmail.com>
2026-02-10 14:36:49 +0700

    lab17_2.cpp completed

--- lab17_2.cpp
+++ lab17_2.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 void myString(char *&p, int N){
 	p = new char[N];    
-	for(int i = 0; i < N;i++) p[i] = 'A'+i;
+	for(int i = 0; i < N;i++) p[i] = 'A' + i ;
 	p[N] = '\0';
 }
 
